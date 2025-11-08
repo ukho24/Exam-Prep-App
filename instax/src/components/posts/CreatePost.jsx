fd9a9e62d797fff6944e75437c36125deba73316
Lutho Mali <ukhomali@icloud.com>
2025-11-08 15:51:49 +0000

    Updates

--- instax/src/components/posts/CreatePost.jsx
+++ instax/src/components/posts/CreatePost.jsx
@@ -1,12 +1,14 @@
-import {Create, SimpleForm, NumberInput, TextInput, DateInput, required, DateTimeInput} from "react-admin"
+import {Create, SimpleForm, AutocompleteInput, TextInput, DateInput, required, DateTimeInput, ReferenceInput} from "react-admin"
 
 export const PostCreate = () => (
     <Create>
         <SimpleForm>
-            <NumberInput source="user_id"/>
+            <ReferenceInput source="user_id" reference="USERS">
+                <AutocompleteInput optionText="username" />
+            </ReferenceInput>
             <TextInput source="title" validate={[required()]} />
             <TextInput source="content" multiline={true} label="What are you thinking...?" validate={[required()]} />
-            <DateTimeInput label="Date" source="created_at" defaultValue={new Date()} disabled />
+            <DateTimeInput label="Date" source="created_at" defaultValue={new Date()} disabled validate={[required()]} />
         </SimpleForm>
     </Create>
 );
